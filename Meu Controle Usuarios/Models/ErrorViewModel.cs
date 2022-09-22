6633511e81e33c90ed2bcfe97897e88c0311f1ed
Carlos Eduardo <86268622+ecadu@users.noreply.github.com>
2022-09-22 09:40:52 -0300

    Add files via upload

---
+++ Meu Controle Usuarios/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Meu_Controle_Usuarios.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
